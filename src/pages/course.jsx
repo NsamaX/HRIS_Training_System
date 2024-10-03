202774dc21799e6070f6df805afba95d62234920
NsamaX <vijuksama@gmail.com>
2024-10-03 13:27:17 +0700

    .

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -3,21 +3,28 @@ import { useLocation } from 'react-router-dom';
 import Course from '../components/course';
 
 const CoursesPage = () => {
-  const [course, setCourse] = useState(null);
+  const [course, setCourse] = useState(null); 
   const location = useLocation();
 
   const getCourseIdFromUrl = () => {
     const searchParams = new URLSearchParams(location.search);
-    return searchParams.get('id');
+    return searchParams.get('id'); 
   };
 
   useEffect(() => {
     const courseId = getCourseIdFromUrl();
     
-    if (courseId) { 
+    if (courseId) {
       fetch(`http://localhost:5000/api/courses?id=${courseId}`)
         .then(response => response.json())
-        .then(data => setCourse(data))
+        .then(data => {
+          console.log('Fetched course data:', data); 
+          if (Array.isArray(data) && data.length > 0) {
+            setCourse(data[0]);
+          } else {
+            console.error('Course data is not in expected format');
+          }
+        })
         .catch(error => console.error('Error fetching course data:', error));
     }
   }, [location]);
